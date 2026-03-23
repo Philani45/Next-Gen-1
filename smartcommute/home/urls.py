28bb769f53c91198f03df26f043a903cf8848ca5
victoriathii <victoriathii@icloud.com>
2026-03-22 20:09:42 -0400

    DJANGO
    all html and css files transfer to Django

---
+++ smartcommute/home/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('schedule/', views.schedule, name='schedule'),
+    path('location/', views.location, name='location'),
+    path('route/', views.route, name='route'),
+    path('departure/', views.departure, name='departure'),
+    path('notifications/', views.notifications, name='notifications'),
+    path('settings/', views.settings, name='settings'),
+    path('datastorage/', views.datastorage, name='datastorage'),
+]
---
+++ smartcommute/home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'home/index.html')
+
+def register(request):
+    return render(request, 'home/register.html')
+
+def schedule(request):
+    return render(request, 'home/schedule.html')
+
+def location(request):
+    return render(request, 'home/location.html')
+
+def route(request):
+    return render(request, 'home/route.html')
+
+def departure(request):
+    return render(request, 'home/departure.html')
+
+def notifications(request):
+    return render(request, 'home/notifications.html')
+
+def settings(request):
+    return render(request, 'home/settings.html')
+
+def datastorage(request):
+    return render(request, 'home/datastorage.html')
